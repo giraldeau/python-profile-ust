eb7c61f433df92b38b4506cb2bc98b1e5dbf9925
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-12-03 16:23:02 -0500

    Add example of profile cython module
    We need to add compiler_directives={'profile': True} to cythonize(), and the
    instrumentation of the main interpreter will be called the same way as other
    interpreted code.
    
    Thanks to Stefan Behnel for this tip!
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- cython/main.py
+++ cython/main.py
@@ -1,7 +1,8 @@
-import foo1, foo2
+import foo1, foo2, foo3
 
 import lttngProfile
 lttngProfile.enable()
 foo1.foo()
 foo2.foo()
+foo3.foo()
 lttngProfile.disable()
--- cython/setup.py
+++ cython/setup.py
@@ -4,13 +4,19 @@ from distutils.extension import Extension
 
 ext = [
     Extension('foo1', ['foo1.pyx'],
-              extra_compile_args = ['-O2', '-g'],
+              extra_compile_args = ['-g'],
     ),
     Extension('foo2', ['foo2.pyx'],
-              extra_compile_args = ['-O2', '-finstrument-functions', '-g'],
+              extra_compile_args = ['-finstrument-functions', '-g'],
+    ),
+]
+
+ext_trace = [
+    Extension('foo3', ['foo3.pyx'],
     ),
 ]
 
 setup(
-    ext_modules = cythonize(ext),
+    ext_modules = cythonize(ext) +
+                  cythonize(ext_trace, compiler_directives={'profile': True}),
 )
