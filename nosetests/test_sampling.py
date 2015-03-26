808f8a0dac076f1b23ce8baf8052ca96650bc4f9
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-03-26 16:54:34 -0400

    Assert that the event list is read-only
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- nosetests/test_sampling.py
+++ nosetests/test_sampling.py
@@ -1,6 +1,9 @@
 from linuxProfile.api import sampling
-from nose.tools import assert_equals
+from nose.tools import assert_equals, assert_raises
+
+def modify_events():
+    sampling.events['hello'] = 42
 
 def test_import_sampling():
-    print(sampling.events)
     assert_equals(len(sampling.events.keys()), 60)
+    assert_raises(TypeError, modify_events)
